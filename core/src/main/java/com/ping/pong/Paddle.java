8397e0cb5aa2e4423cd34b2b463021642ee66604
Aarnav Tale <aarnavxtale@gmail.com>
2021-05-29 13:43:29 -0400

    [Project] Completed Game

--- core/src/main/java/com/ping/pong/Paddle.java
+++ core/src/main/java/com/ping/pong/Paddle.java
@@ -1,30 +1,50 @@
 package com.ping.pong;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import org.mini2Dx.core.graphics.Graphics;
 
 
 public class Paddle {
 
-    // variables here
+    Vector2 pos, dimension;
+
+    int speed = 500;
 
     public Paddle(Vector2 pos, Vector2 dimension) {
 
+        this.pos = pos;
+        this.dimension = dimension;
 
     }
 
     public void update(float delta, boolean up, boolean down) {
-        // move paddle and collide with ball
 
+        int movement = 0;
+        if (up) movement = -1;
+        if (down) movement = 1;
 
+        pos.y += speed * movement * delta;
+        pos.y = Math.max(Math.min(pos.y, PingPong.HEIGHT - dimension.y/2 - 10), dimension.y/2 + 10);
+
+        Ball ball = PingPong.ball;
+        if (Math.signum(ball.vel.x) == Math.signum(pos.x - PingPong.WIDTH/2F) && collidesWith(ball)) {
+            ball.vel.x *= -1.2F;
+            ball.vel.y += speed * movement;
+            ball.vel.y = Math.min(400, Math.max(-400, ball.vel.y));
+        }
     }
 
     public boolean collidesWith(Ball ball) {
-        return false;
+        return (ball.pos.x + ball.radius >= pos.x - dimension.x/2 &&
+                ball.pos.x - ball.radius <= pos.x + dimension.x/2 &&
+                ball.pos.y + ball.radius >= pos.y - dimension.y/2 &&
+                ball.pos.y - ball.radius <= pos.y + dimension.y/2);
     }
 
     public void render(Graphics g) {
-        // render paddle
 
+        g.setColor(Color.WHITE);
+        g.fillRect((int)(pos.x - dimension.x / 2f), (int)(pos.y - dimension.y / 2f), dimension.x, dimension.y);
     }
 }
--- core/src/main/java/com/ping/pong/PingPong.java
+++ core/src/main/java/com/ping/pong/PingPong.java
@@ -1,43 +1,50 @@
 package com.ping.pong;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.ping.pong.input.KeyListener;
 import com.ping.pong.input.Keys;
+import org.mini2Dx.core.font.GameFont;
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
 
 public class PingPong extends BasicGame {
-
-    // basic setup and input stuff
 	public static final String GAME_IDENTIFIER = "com.ping.pong";
 	public static int WIDTH, HEIGHT;
 
 	private Keys keys;
+	private Paddle left, right;
 
-	// object variables
+	public static Ball ball;
 
-
-
+	public static int leftScore, rightScore;
+	public static int nextBallSpawnDirection = 1;
 
 	public static void scoreLeft() {
-
+	    leftScore++;
+	    score();
     }
 
     public static void scoreRight() {
-
+        rightScore++;
+        score();
     }
 
     public static Vector2 center() {
-        return null;
+	    return new Vector2(WIDTH/2F, HEIGHT/2F);
     }
 
     public static Vector2 initBallVel() {
-	    return null;
+	    Vector2 vel = new Vector2(150 * nextBallSpawnDirection, 150);
+	    nextBallSpawnDirection *= -1;
+	    return vel;
     }
 
     public static void score() {
-
+	    ball.pos = center();
+	    ball.vel = initBallVel();
     }
 
 	private int leftPaddleKey = 0, rightPaddleKey = 0;
@@ -50,9 +57,10 @@ public class PingPong extends BasicGame {
         WIDTH = Gdx.graphics.getWidth();
         HEIGHT = Gdx.graphics.getHeight();
 
-        // create objects
+        ball = new Ball(center(), initBallVel());
 
-
+        left = new Paddle(new Vector2(40, HEIGHT / 2), new Vector2(20, 100));
+        right = new Paddle(new Vector2(WIDTH - 40, HEIGHT / 2), new Vector2(20, 100));
     }
     
     @Override
@@ -64,7 +72,10 @@ public class PingPong extends BasicGame {
 
         // your update code here ======
 
+        ball.update(delta);
 
+        left.update(delta, keys.down(Input.Keys.W), keys.down(Input.Keys.S));
+        right.update(delta, keys.down(Input.Keys.UP), keys.down(Input.Keys.DOWN));
 	}
     
     @Override
@@ -76,7 +87,15 @@ public class PingPong extends BasicGame {
     @Override
     public void render(Graphics g) {
 	    // draw to the screen
+        g.setColor(Color.WHITE);
+        g.drawLineSegment(WIDTH / 2, 0, WIDTH / 2, HEIGHT);
+        g.drawString("" + leftScore, WIDTH * 1f / 4, 20);
+        g.drawString("" + rightScore, WIDTH * 3f / 4, 20);
 
+        ball.render(g);
+
+		left.render(g);
+		right.render(g);
 
 	}
 }
