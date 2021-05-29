ec2e47c3df266a14cb26ef24a4e74f65f2a231b7
Aarnav Tale <aarnavxtale@gmail.com>
2021-05-29 13:43:29 -0400

    [Project] Completed Game

--- core/src/main/java/com/ping/pong/Ball.java
+++ core/src/main/java/com/ping/pong/Ball.java
@@ -1,25 +1,30 @@
 package com.ping.pong;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import org.mini2Dx.core.graphics.Graphics;
 
 public class Ball {
 
-    // variable here
+    public float radius = 10;
+    public Vector2 pos, vel;
 
     public Ball(Vector2 pos, Vector2 vel) {
-
+        this.pos = pos;
+        this.vel = vel;
     }
 
     public void update(float delta) {
-        // move ball, collide with top and bottom, and check if ball enters either goal
+        pos.add(vel.cpy().scl(delta));
 
+        if (pos.y - radius <= 0 || pos.y + radius >= PingPong.HEIGHT) vel.scl(new Vector2(1, -1));
 
+        if (pos.x + radius <= 0) PingPong.scoreRight();
+        if (pos.x - radius >= PingPong.WIDTH) PingPong.scoreLeft();
     }
 
     public void render(Graphics g) {
-        // render ball
-
-
+        g.setColor(Color.WHITE);
+        g.fillCircle(pos.x, pos.y, radius);
     }
 }
